c135f14e3d875387df9bd8511a81bf85d56eafde
ram <bvn.ramkumar@gmail.com>
2025-08-28 22:49:01 +0530

    slider

--- src/components/BreakingNewsSlider.jsx
+++ src/components/BreakingNewsSlider.jsx
@@ -26,7 +26,7 @@ const BreakingNewsSlider = ({ articles }) => {
   };
 
   return (
-    <div className="my-6">
+    <div className="my-6 bg-gray-400">
       <h2 className="text-xl font-bold mb-4 text-red-600">
          Breaking News
       </h2>
