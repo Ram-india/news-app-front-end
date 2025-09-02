8e1a9c19dc8a693ce8483d60b35b0f8752644bd6
ram <bvn.ramkumar@gmail.com>
2025-09-02 16:41:14 +0530

    masonary added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
-import Newscard from "../components/Newscard";
-import API from "../services/axios";
-import BreakingNewsSlider from "../components/BreakingNewsSlider";
-import TickerBreakingNews from "../components/TickerBreakingNews";
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar';
+import Newscard from '../components/Newscard';
+import API from '../services/axios';
+import BreakingNewsSlider from '../components/BreakingNewsSlider';
+import TickerBreakingNews from '../components/TickerBreakingNews';
 import Masonry from "react-masonry-css";
 
+
+
 const Home = () => {
-  const [articles, setArticles] = useState([]);
+  const[articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const fetchPersonalizedNews = async () => {
     try {
       const res = await API.get("/news/personalized");
-      console.log("API Response:", res.data);
+      console.log("API Response:", res.data); 
       setArticles(res.data.articles || []);
     } catch (err) {
       console.error("Failed to fetch personalized news:", err);
@@ -20,15 +22,15 @@ const Home = () => {
       setLoading(false);
     }
   };
-
+  
   useEffect(() => {
     fetchPersonalizedNews();
   }, []);
-  // Breakpoints for masonry
-  const breakpointColumnsObj = {
+   // Breakpoints for masonry
+   const breakpointColumnsObj = {
     default: 3, // 3 columns on large screens
-    1100: 2, // 2 columns on medium
-    700: 1, // 1 column on small
+    1100: 2,    // 2 columns on medium
+    700: 1      // 1 column on small
   };
   return (
     <>
@@ -37,25 +39,25 @@ const Home = () => {
           <p>Loading News..</p>
         ) : (
           <>
-            <TickerBreakingNews />
+           <TickerBreakingNews/>
             {/* Breaking News Slider */}
             <BreakingNewsSlider articles={articles} />
-
+  
             {/* Articles Grid */}
             <Masonry
-              breakpointCols={breakpointColumnsObj}
-              className="flex gap-4 px-4 py-2"
-              columnClassName="bg-clip-padding"
-            >
-              {articles.map((article, index) => (
-                <Newscard key={index} article={article} />
-              ))}
-            </Masonry>
+  breakpointCols={breakpointColumnsObj}
+  className="my-masonry-grid"
+  columnClassName="my-masonry-grid_column"
+>
+  {articles.map((article, index) => (
+    <Newscard key={index} article={article} />
+  ))}
+</Masonry> 
           </>
         )}
       </div>
     </>
   );
-};
+}
 
-export default Home;
+export default Home
