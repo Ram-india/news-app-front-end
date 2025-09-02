d3f2526b5c28ef4a255a9119098a55e26fe25129
ram <bvn.ramkumar@gmail.com>
2025-09-02 16:34:57 +0530

    masonary

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar';
-import Newscard from '../components/Newscard';
-import API from '../services/axios';
-import BreakingNewsSlider from '../components/BreakingNewsSlider';
-import TickerBreakingNews from '../components/TickerBreakingNews';
-
-
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Newscard from "../components/Newscard";
+import API from "../services/axios";
+import BreakingNewsSlider from "../components/BreakingNewsSlider";
+import TickerBreakingNews from "../components/TickerBreakingNews";
+import Masonry from "react-masonry-css";
 
 const Home = () => {
-  const[articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const fetchPersonalizedNews = async () => {
     try {
       const res = await API.get("/news/personalized");
-      console.log("API Response:", res.data); 
+      console.log("API Response:", res.data);
       setArticles(res.data.articles || []);
     } catch (err) {
       console.error("Failed to fetch personalized news:", err);
@@ -21,10 +20,16 @@ const Home = () => {
       setLoading(false);
     }
   };
-  
+
   useEffect(() => {
     fetchPersonalizedNews();
   }, []);
+  // Breakpoints for masonry
+  const breakpointColumnsObj = {
+    default: 3, // 3 columns on large screens
+    1100: 2, // 2 columns on medium
+    700: 1, // 1 column on small
+  };
   return (
     <>
       <div>
@@ -32,23 +37,25 @@ const Home = () => {
           <p>Loading News..</p>
         ) : (
           <>
-           <TickerBreakingNews/>
+            <TickerBreakingNews />
             {/* Breaking News Slider */}
             <BreakingNewsSlider articles={articles} />
-  
+
             {/* Articles Grid */}
-            <div className="container  px-8 mx-auto">
-              <div className="grid gap-6 grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 p-4">
-                {articles.map((article, index) => (
-                  <Newscard key={index} article={article} />
-                  ))}
-              </div>
-            </div>
+            <Masonry
+              breakpointCols={breakpointColumnsObj}
+              className="flex gap-4"
+              columnClassName="bg-clip-padding"
+            >
+              {articles.map((article, index) => (
+                <Newscard key={index} article={article} />
+              ))}
+            </Masonry>
           </>
         )}
       </div>
     </>
   );
-}
+};
 
-export default Home
+export default Home;
