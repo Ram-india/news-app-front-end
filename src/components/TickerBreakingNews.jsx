157e96e63a996cc6a9b4890e2246cb591a19ea18
ram <bvn.ramkumar@gmail.com>
2025-09-03 10:20:20 +0530

    marquee correction

--- src/components/TickerBreakingNews.jsx
+++ src/components/TickerBreakingNews.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TickerBreakingNews = ({articles}) => {
   return (
     <div className='bg-red-600 text-white py-2 px-4 text-sm font-medium flex items-center'>
-        <span className="mr-2 font-bold "> Breaking news</span>
+        <span className="mr-2 font-bold  w-full"> Breaking news</span>
         {
             articles.map((article, index) => (
                 <marquee >{article.title}</marquee>
